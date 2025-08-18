1002c497c8cdceae8926bc8bfaf8d8eeb6b4ad69
BuiChiBao <buichibao1601@gmail.com>
2025-08-18 15:54:41 +0700

    feat: add redis service

---
+++ src/services/redis.service.js
@@ -0,0 +1,54 @@
+"use strict"
+
+const redis = require("redis")
+const { setTimeout } = require("timers/promises")
+const { reservationInventory } = require("../model/repositories/invetory.repo")
+
+const redisClient = redis.createClient()
+
+redisClient.on("error", (err) => console.error("Redis Client Error", err))
+
+// Kết nối redis
+;(async () => {
+  await redisClient.connect()
+})()
+
+const acquireLock = async (productId, quantity, cartId) => {
+  const key = `lock_v2025_${productId}`
+  const retryTime = 10
+  const expireTime = 3000 // ms
+
+  for (let i = 0; i < retryTime; i++) {
+    // setNX trả về true nếu set thành công
+    const result = await redisClient.setNX(key, expireTime)
+
+    if (result) {
+      // thao tác với inventory
+      const isReservation = await reservationInventory({
+        productId,
+        quantity,
+        cartId
+      })
+
+      if (isReservation.modifiedCount) {
+        // đặt expire cho key
+        await redisClient.pExpire(key, expireTime)
+        return key
+      }
+
+      return null
+    } else {
+      // chờ 50ms rồi thử lại
+      await setTimeout(50)
+    }
+  }
+}
+
+const releaseLock = async (keyLock) => {
+  return await redisClient.del(keyLock)
+}
+
+module.exports = {
+  acquireLock,
+  releaseLock
+}
