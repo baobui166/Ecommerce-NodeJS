91785d5765eba84d1a2ce585ebc29253a9ae5b95
BuiChiBao <buichibao1601@gmail.com>
2025-11-04 22:22:04 +0700

    fix: update class productFactoryV2

--- src/services/product.service.xxx.js
+++ src/services/product.service.xxx.js
@@ -19,6 +19,7 @@ const {
   updateProductById
 } = require("../model/repositories/product.repo")
 const { removeUndefinedObject, updateNestedObject } = require("../utils")
+const NotificationService = require("./notification.service")
 
 // define Factory class to create product
 class ProductFactoryV2 {
@@ -120,11 +121,25 @@ class Product {
     const newProduct = product.create({ ...this, _id: product_id })
 
     if (newProduct) {
+      // add product_ids in inventory collection
       await insertInventory({
         product_id: newProduct._id,
         shopId: this.product_shop,
         stock: this.product_quantity
       })
+
+      // push notification to system collection
+      NotificationService.createNotiSystem({
+        type: "SHOP-001",
+        receivedId: 1,
+        senderId: this.product_shop,
+        options: {
+          product_name: this.product_name,
+          shop_name: this.product_shop
+        }
+      })
+        .then((rs) => console.log(rs))
+        .catch(console.error)
     }
 
     return newProduct
