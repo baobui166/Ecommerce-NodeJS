0b3515c552ac390347c69d7a7632b24fdbf5639d
BuiChiBao <buichibao1601@gmail.com>
2025-11-04 22:20:39 +0700

    feat: inventory feature

--- src/model/repositories/invetory.repo.js
+++ src/model/repositories/invetory.repo.js
@@ -1,5 +1,6 @@
 "use strict"
 
+const { convertToObjectIdMongodb } = require("../../utils")
 const { inventory } = require("../inventory.model")
 
 const insertInventory = async ({ productId, shopId, stock, location }) => {
@@ -11,4 +12,18 @@ const insertInventory = async ({ productId, shopId, stock, location }) => {
   })
 }
 
-module.exports = { insertInventory }
+const reservationInventory = async ({ productId, quantity, cartId }) => {
+  const query = {
+      inven_productId: convertToObjectIdMongodb(productId),
+      inven_stock: { $gte: quantity }
+    },
+    updateSet = {
+      $inc: { inven_stock: -quantity },
+      $push: { inven_reservations: { quantity, cartId, createOn: new Date() } }
+    },
+    options = { upsert: true, new: true }
+
+  return await inventory.updateOne(query, updateSet, options)
+}
+
+module.exports = { insertInventory, reservationInventory }
